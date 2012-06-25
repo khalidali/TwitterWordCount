7e0554bfd77b3754f86c273bc30a52f5f873a4ee
saasbook <saasbook@saasbook.(none)>
2012-06-25 16:09:56 -0700

    updated code to get more then 200 tweets

--- twitter.rb
+++ twitter.rb
@@ -3,19 +3,26 @@ require 'json'
 
 #TwitterWordFrequency.new("2", "BarakObama")
 
+COUNT = 10
+
 class TwitterWordFrequency
 
-  attr_accessor :count, :screen_name
+  attr_accessor :count, :screen_name, :words
   
   def initialize count, screen_name
     @count = count
     @screen_name = screen_name
   end
   
-  def getTweets
+  def getTweets max_id
     uri = URI "https://api.twitter.com/1/statuses/user_timeline.json"
-    params = {:count => @count, :screen_name => @screen_name, :trim_user => true}
+    if max_id
+      params = {:count => COUNT, :screen_name => @screen_name, :trim_user => true, :max_id => max_id}
+    else
+      params = {:count => COUNT, :screen_name => @screen_name, :trim_user => true}
+    end
     uri.query = URI.encode_www_form(params)
+    
     
     Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
       request = Net::HTTP::Get.new uri.request_uri
@@ -25,31 +32,30 @@ class TwitterWordFrequency
     end
   end
   
-  def parseTweets
-    words = {}
-    tweets = JSON.parse(getTweets)
-    tweets.each do |tweet|
-      tweet["text"].downcase.split(" ").each do |word|
-        word = word.gsub(/\W/, "")
-        if(words.has_key? word)
-          words[word] += 1
-        elsif(word != "") 
-          words[word] = 1
-        end 
-      end
-    end
-    return words
-  end
-  
-  def words
+  def getWordCount 
+    @words = {}
+    count = 0
+    max_id = nil
     
-  
-  
-  
+    
+    while count < @count
+      
+      tweets = JSON.parse(getTweets max_id)
+      
+      tweets.each do |tweet|
+        tweet["text"].downcase.split(" ").each do |word|
+          word = word.gsub(/\W/, "")
+          if(words.has_key? word)
+            words[word] += 1
+          elsif(word != "") 
+            words[word] = 1
+          end 
+        max_id = tweet["id"]
+        end
+      end
+      count += COUNT
+    end
+    return words.sort_by { |word, count| count}
   end
-  
-  
-  
-  
 end
 
